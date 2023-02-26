b8c16c47ac88a8255bf6819a0e36988e0c057203
Atthie Mulongo <atthie27@gmail.com>
2023-02-26 19:54:51 +0100

    Creation du script pour le slide

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const menu = document.querySelector('.menu-icone');
+const gauche =document.querySelector('.fleche-gauche');
+const droite =document.querySelector('.fleche-droite');
+const slide = ["p1.jpg", "p2.png", "p3.png"];
+let numero = 0;
 menu.addEventListener('click',() =>{
     const lien =document.querySelector('.lien');
     const visibility =lien.getAttribute('style');
@@ -6,6 +10,15 @@ menu.addEventListener('click',() =>{
         lien.style.visibility='visible';
     }else{
         lien.style.visibility='hidden';
-
     }
 })
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.querySelector(".slide").src = slide[numero];
+    console.log(numero);
+}
